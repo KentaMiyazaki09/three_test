2456f9d2f22c4b2f427d3a1064d490a99a0e5434
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2024-07-23 19:01:58 +0900

    Add click

--- src/assets/js/scrollZ.js
+++ src/assets/js/scrollZ.js
@@ -17,17 +17,6 @@ import * as THREE from 'three';
 // })
 // GUIここまで
 
-// 慣性スクロールの値
-let inertialScroll = 0
-// 慣性スクロールのパーセント値(0~100)
-let inertialScrollPercent = 0
-
-let scene, camera, renderer
-
-let sphere = null
-let sphereColor = null
-let canvas  = null
-
 function random (num, last=100) {
   return Math.random() * num * 2 - last
 }
@@ -40,12 +29,19 @@ function lerp(x, y, a) {
   return x + (y - x) * easeOutQuad(a)
 }
 
-/** パーセントのスケール */
+// 慣性スクロールの値
+let inertialScroll = 0
+// 慣性スクロールのパーセント値(0~100)
+let inertialScrollPercent = 0
+
+/**
+ * パーセントのスケール
+ */
 function scalePercent(start, end) {
   return (inertialScrollPercent - start) / (end - start);
 }
 
-/** スクロール中の動き */
+let scene, camera, renderer
 const animationScripts = [
   {
     start: 0,
@@ -57,7 +53,9 @@ const animationScripts = [
   },
  ]
 
-/** 慣性スクロールのためにスクロール値を取得する*/
+ /**
+   * 慣性スクロールのためにスクロール値を取得する
+   */
 function setScrollPercent() {
   inertialScroll +=
     ((document.documentElement.scrollTop || document.body.scrollTop) - inertialScroll) * 0.08;
@@ -106,21 +104,73 @@ function createBox () {
   }
 }
 
+let sphere = null
+let sphereColor = null
 function createSphere() {
   const geometry = new THREE.SphereGeometry(2, 32, 16)
   const material = new THREE.MeshLambertMaterial({ color: 'rgb(144, 196, 255)' })
   sphere = new THREE.Mesh(geometry, material)
+  sphere.name = 'blue sphere'
   sphere.position.set(0, 0, -8)
   scene.add(sphere)
 }
 
+const raycaster = new THREE.Raycaster()
+let moveFlg = false
+let mouse = null
+// マウスの座標を記録
+function getMousePos() {
+  mouse = new THREE.Vector2()
+  canvas.addEventListener('mousemove', (event) => {
+    const element = event.currentTarget
+
+    // canvas上のXY座標
+    const x = event.clientX - element.offsetLeft
+    const y = event.clientY - element.offsetTop
+
+    // canvas要素の幅・高さ
+    const w = element.offsetWidth
+    const h = element.offsetHeight
+
+    mouse.x = (x / w) * 2 - 1
+    mouse.y = -(y / h) * 2 + 1
+  })
+}
+
+// ホバーイベントをチェック
+function checkHover(mouse, camera) {
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.intersectObjects(scene.children)
+
+  // blue sphereのマウスホバー
+  if (intersects.length > 0 && intersects[0].object.name === 'blue sphere') {
+    moveFlg = true
+    sphere.material.color.setRGB(0, 0, 0.1)
+  } else {
+    moveFlg = false
+    sphere.material.color.setRGB(sphereColor.r, sphereColor.g, sphereColor.b)
+  }
+}
+
+// クリックイベントをチェック
+function handleClick() {
+  canvas.addEventListener('click', () => {
+    // blue sphereのクリックイベント
+    if (moveFlg) {
+      window.open('https://www.rhino-inc.jp/')
+    }
+  })
+}
+
 function render() {
+  checkHover(mouse, camera)
   renderer.render(scene, camera)
   setScrollPercent()
   window.requestAnimationFrame(render)
   playScrollAnimations()
 }
 
+let canvas  = null
 function init() {
   canvas = document.querySelector('#bg')
   scene = new THREE.Scene()
@@ -139,6 +189,16 @@ function init() {
 
   createBox()
   createSphere()
+
+  sphereColor = {
+    r: sphere.material.color.r,
+    g: sphere.material.color.g,
+    b: sphere.material.color.b,
+  }
+
+  getMousePos()
+
+  handleClick()
 
   render()
 }
